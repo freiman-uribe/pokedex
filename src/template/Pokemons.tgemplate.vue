32e6dba50eb4ffb767f2cdaceb39f595a9e858b2
Freiman Uribe <freimanuribe15@gmail.com>
2026-01-31 07:56:12 -0500

    Se agrega la estructura de template para conservar elemntos en vistas compratidas y se agrega funcion de rutas activas

---
+++ src/template/Pokemons.tgemplate.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import BaseHeader from '../components/BaseHeader.vue';
+import BaseFooter from '../components/BaseFooter.vue';
+</script>
+<template>
+    
+    <div class="layout-wrapper">
+        <base-header />
+        <slot name="content" />
+        <slot name="details" />
+    </div>
+    <base-footer />
+
+</template>
